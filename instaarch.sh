e15cc260d2b6e012fe311b0e788740be135ce254
ThePacificGamer <48371085+ThePacificGamer@users.noreply.github.com>
2021-11-11 16:21:33 -0800

    debug and tilix addition

--- instaarch.sh
+++ instaarch.sh
@@ -21,7 +21,7 @@ cd paru
 makepkg -si
 cd
 clear
-echo "type yay -S <package> to use the AUR"
+echo "type paru -S <package> to use the AUR"
 sleep 3
 #installing xorg+nvidia drivers
 echo "installing XFCE and Nvidia Drivers"
@@ -38,11 +38,11 @@ sudo pacman -S --noconfirm sddm
 sleep 2
 #getting the games
 echo "getting game launchers and other stuff"
-yay -S discord
+paru -S discord
 sudo pacman -S --noconfirm wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox 
-sudo pacman -S --noconfirm steam lutris
+sudo pacman -S --noconfirm steam lutris  tilix
 sudo pacman -S --noconfirm firefox
-yay -S all-repository-fonts
+paru -S all-repository-fonts
 clear
 echo "Finishing up! We are almost done!"
 sleep 2
